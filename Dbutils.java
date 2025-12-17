f3536a9bb55f1bb87386a9021aa7ead9e67f5a8e
sireesha-dotcom <sireeshasiri1796@gmail.com>
2025-12-17 22:42:27 +0000

    first commit

---
+++ Dbutils.java
@@ -0,0 +1,5 @@
+Hello 
+Hi 
+how  are you 
+Hello git commands are more imp do more practice on git like linux
+
---
+++ stringutils.java
@@ -0,0 +1,4 @@
+hello 
+i am good to learn devops tools 
+i will able to change the domain 
+
